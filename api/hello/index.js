cbb4d2ae2100e717976b7784244ce108e6a36e17
Prakash Sambasivam <connect.prakashsap@gmail.com>
2025-07-15 12:03:08 +0530

    Fix Azure Function folder structure

--- api/hello/index.js
+++ api/hello/index.js
@@ -1,5 +1,7 @@
+// api/hello/index.js
 module.exports = async function (context, req) {
   context.res = {
+    status: 200,
     headers: { "Content-Type": "application/json" },
     body: { message: "Hello from Azure API!" },
   };
